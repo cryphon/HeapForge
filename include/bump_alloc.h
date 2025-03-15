1f5554dd877e09d04a60916476787302c9437591
cryphon <yvanroes@gmail.com>
2025-03-15 23:42:22 +0000

    created implementation of bump-up allocator

--- include/bump_alloc.h
+++ include/bump_alloc.h
@@ -1,10 +1,20 @@
 #ifndef BUMP_ALLOC_H
 #define BUMP_ALLOC_H
 #include <stdint.h>
+#include <stdlib.h>
 
 #define BUMP_ALLOC_OK        0  // Success
 #define BUMP_ALLOC_ENOMEM   -1  // Not enough memory
 #define BUMP_ALLOC_EFULL    -2  // Allocator fully used
 #define BUMP_ALLOC_EINVAL   -3  // Invalid request
 
+typedef struct {
+    char* start;    // start if memory region
+    char* curr;     // cyrrent allocation position
+    char* end;      // end of memory region
+} bump_allocator;
+
+void bump_init(bump_allocator* allocator, void* memory, size_t size);
+void* bump_alloc(bump_allocator* allocator, size_t size);
+void bump_free(bump_allocator* allocator);
 #endif
--- src/bump_alloc.c
+++ src/bump_alloc.c
@@ -1,1 +1,26 @@
 #include "bump_alloc.h"
+
+void bump_init(bump_allocator* allocator, void* memory, size_t size) {
+    allocator->start = (char*)memory;
+    allocator->curr = (char*)memory;
+    allocator->end = allocator->start + size;
+}
+
+void* bump_alloc(bump_allocator* allocator, size_t size) {
+    if(allocator->curr + size > allocator->end) { 
+        return NULL;
+    }
+    // save curr position as result
+    void* res = allocator->curr;
+
+    // bump pointer
+    allocator->curr+= size;
+    return res;
+}
+
+void bump_reset(bump_allocator* allocator) {
+    allocator->curr = allocator->start;
+}
+
+
+
